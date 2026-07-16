7c96d24ddc10f2eeead0b3625f8796959d99f4cd
Kenjo <regenciajoshua123@gmail.com>
2026-07-17 00:08:41 +0800

    remove base controller

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Settings;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Settings\ProfileDeleteRequest;
 use App\Http\Requests\Settings\ProfileUpdateRequest;
 use App\Models\User;
@@ -16,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class ProfileController extends Controller
+class ProfileController
 {
     /**
      * Show the user's profile settings page.
--- app/Http/Controllers/Settings/SecurityController.php
+++ app/Http/Controllers/Settings/SecurityController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Settings;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Settings\PasswordUpdateRequest;
 use App\Http\Requests\Settings\TwoFactorAuthenticationRequest;
 use App\Models\User;
@@ -15,7 +14,7 @@ use Inertia\Inertia;
 use Inertia\Response;
 use Laravel\Fortify\Features;
 
-class SecurityController extends Controller
+class SecurityController
 {
     /**
      * Show the user's security settings page.
